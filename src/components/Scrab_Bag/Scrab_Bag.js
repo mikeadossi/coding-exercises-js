961dbb3403f507ad5e4e80b1ead82a244234518b
mikeadossi <mikeadossi@gmail.com>
2017-04-19 14:29:20 -0700

    standstill on Scrab_bag

--- src/components/Scrab_Bag/Scrab_Bag.js
+++ src/components/Scrab_Bag/Scrab_Bag.js
@@ -13,18 +13,18 @@ export default class Scrab_Bag extends React.Component{
       displayRow: 'display-scrabble-row',
       display_reveal_row: 'hide',
       revealPieces: 'revealScrabble',
-      returnPieces: 'hide'
+      returnPieces: 'hide',
+      userScrabbleWordEntry: ''
     }
 
     this.revealPieces = this.revealPieces.bind(this);
     this.returnScrabblePieces = this.returnScrabblePieces.bind(this);
+    this.accountForPieceRemoval = this.accountForPieceRemoval.bind(this);
     // this.PopulateScrabblePieces = PopulateScrabblePieces.bind(this);
   }
 
   revealPieces(e){
     e.preventDefault()
-    console.log('revealed!!!')
-    // scrabble-backside-row
     this.setState({
       displayRow : 'hide',
       display_reveal_row : 'display-scrabble-row',
@@ -39,14 +39,65 @@ export default class Scrab_Bag extends React.Component{
 
   returnScrabblePieces(e){
     e.preventDefault()
-    console.log('not revealed')
-    // scrabble-backside-row
     this.setState({
       displayRow : 'display-scrabble-row',
       display_reveal_row : 'hide',
       revealPieces : 'revealScrabble',
       returnPieces : 'hide'
     })
+  }
+
+  accountForPieceRemoval(e){
+    e.preventDefault()
+    const totalScrabblePieces = {
+      A : { character: 'A', pieceCount: 9, presentCount: 9 },
+      B : { character: 'B', pieceCount: 2, presentCount: 2},
+      C : { character: 'C', pieceCount: 2, presentCount: 2},
+      D : { character: 'D', pieceCount: 4, presentCount: 4},
+      E : { character: 'E', pieceCount: 12, presentCount: 12},
+      F : { character: 'F', pieceCount: 2, presentCount: 2},
+      G : { character: 'G', pieceCount: 3, presentCount: 3},
+      H : { character: 'H', pieceCount: 2, presentCount: 2},
+      I : { character: 'I', pieceCount: 9, presentCount: 9},
+      J : { character: 'J', pieceCount: 1, presentCount: 1},
+      K : { character: 'K', pieceCount: 1, presentCount: 1},
+      L : { character: 'L', pieceCount: 4, presentCount: 4},
+      M : { character: 'M', pieceCount: 2, presentCount: 2},
+      N : { character: 'N', pieceCount: 6, presentCount: 6},
+      O : { character: 'O', pieceCount: 8, presentCount: 8},
+      P : { character: 'P', pieceCount: 2, presentCount: 2},
+      Q : { character: 'Q', pieceCount: 1, presentCount: 1},
+      R : { character: 'R', pieceCount: 6, presentCount: 6},
+      S : { character: 'S', pieceCount: 4, presentCount: 4},
+      T : { character: 'T', pieceCount: 6, presentCount: 6},
+      U : { character: 'U', pieceCount: 4, presentCount: 4},
+      V : { character: 'V', pieceCount: 2, presentCount: 2},
+      W : { character: 'W', pieceCount: 2, presentCount: 2},
+      X : { character: 'X', pieceCount: 1, presentCount: 1},
+      Y : { character: 'Y', pieceCount: 2, presentCount: 2},
+      Z : { character: 'Z', pieceCount: 1, presentCount: 1},
+      none : { character: '', pieceCount: 2, presentCount: 2}
+    }
+    console.log('accounted for')
+    let targetValue = e.target.value
+    let userEntry = []
+    userEntry.push(targetValue)
+
+    this.setState({
+      userScrabbleWordEntry : userEntry
+    })
+    console.log('userEntry[0]: ', userEntry[0])
+    console.log("totalScrabblePieces.userEntry[0]: ", totalScrabblePieces[userEntry[0]])
+    console.log('this.state.userScrabbleWordEntry: ',this.state.userScrabbleWordEntry)
+
+    // for(let i = 0; i < userEntry.length; i++){
+    //   if( totalScrabblePieces[userEntry[i]].presentCount > 0 ){
+    //     totalScrabblePieces[userEntry[i]].presentCount--
+    //   } else {
+    //     console.log('letter cannot be used')
+    //     return
+    //   }
+    // }
   }
 
 
@@ -109,7 +160,7 @@ export default class Scrab_Bag extends React.Component{
 
 
 
-        <input className="scrabbleBoard" placeholder="type the letters you want"/>
+        <input className="scrabbleBoard" onChange={this.accountForPieceRemoval}placeholder="type the letters you want"/>
         <div className="scrabbleBase"></div>
       <button className={this.state.revealPieces} onClick={this.revealPieces}>reveal all at once</button>
     <button className={this.state.returnPieces} onClick={this.returnScrabblePieces}>return</button>
