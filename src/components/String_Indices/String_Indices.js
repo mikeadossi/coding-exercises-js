304da5f72f8133063a1daba66d9dc6f585027574
mikeadossi <mikeadossi@gmail.com>
2017-04-20 22:04:33 -0700

    handle enter keypress on input

--- src/components/String_Indices/String_Indices.js
+++ src/components/String_Indices/String_Indices.js
@@ -16,6 +16,7 @@ export default class String_Indicies extends React.Component{
     this.saveStringIndicesEntries = this.saveStringIndicesEntries.bind(this);
     this.runStringIndexProcess = this.runStringIndexProcess.bind(this);
     this.String_indices_render = String_indices_render.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   saveStringIndicesEntries(e){
@@ -37,6 +38,12 @@ export default class String_Indicies extends React.Component{
     this.refs.ourInput.value = '';
   }
 
+  handleKeyPress(e){
+    if(e.key == 'Enter'){
+      this.runStringIndexProcess()
+    }
+  }
+
   render(){
     let test = this.String_indices_render()
     return(
@@ -47,7 +54,7 @@ export default class String_Indicies extends React.Component{
         <p className="instructions">Write a program that parses the words in a string by providing a numerical index, similar to how you would access an element within an array with a numerical index.</p>
         </div>
         <h2>Enter a String</h2>
-      <input ref="ourInput" className="string_indices_input appInputStyling" onChange={this.saveStringIndicesEntries} placeholder="one string at a time"/>
+      <input onKeyPress={this.handleKeyPress} ref="ourInput" className="string_indices_input appInputStyling" onChange={this.saveStringIndicesEntries} placeholder="one string at a time"/>
       <button className="string_indices_btn appButtonSyling" onClick={this.runStringIndexProcess}>pass in</button>
       <div className="string_indices_results">
         <div className="string_indices_result_div">
