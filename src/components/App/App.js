6f63ca87387c34fda72b855f7356c3be0f264d2f
mikeadossi <mikeadossi@gmail.com>
2017-04-23 02:52:24 -0700

    setting up braille

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,7 +22,7 @@ export default class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      currentPage: "finite_state_gate_page",
+      currentPage: "braille_page",
       guess_random_num: 0,
       guess_input_value: 0,
       guesses_too_small: [],
