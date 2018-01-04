6282403f8d9c9bf58649424c7d1f412454f9af8f
mikeadossi <mikeadossi@gmail.com>
2018-01-04 13:21:54 -0800

    package-lock.json edit

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,10 @@ module.exports = {
     },
     devServer: {
         disableHostCheck: true,
-        //contentBase: './src/', // Relative directory for base of server
         hot: true, // Live-reload
         inline: true,
         port: process.env.PORT, // Port Number
         host: '0.0.0.0', // Change to '0.0.0.0' for external facing server
-        //historyApiFallback: true,
     },
     output: {
         path: path.join(__dirname, 'public'),
